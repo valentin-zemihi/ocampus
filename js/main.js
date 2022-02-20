9bf323cb59cff2c5d706635184acd84b6dc479ba
valentin-zemihi <valentin.zemihi@gmail.com>
2022-02-20 19:12:59 +0100

    Update V2
    Update du site internet avec la mise à jour de la V2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-var wowOpen = true ;
 var isHomePage ;
 var isContactPage = false;
 
@@ -8,9 +7,7 @@ window.onload = function() {
 		isHomePage = true ;
 
 		initializeBDDASSO() ;
-		initializeBDDNEWS() ;
 
-		initializeFiveBigNews() ;
 		initializeAssoHomePage() ;
 	}
 
@@ -20,31 +17,9 @@ window.onload = function() {
 		initializePage() ;
 	}
 
-	if (document.title =="Actualités - ÔCampus")
-	{
-		initializeBDDNEWS() ;
-
-		initializeFiveBigNews() ;
-		initializeNews() ;
-	}
-
 	if (document.title == "Contact - ÔCampus") {isContactPage = true}
 
 	initializeFooter() ;
-}
-
-function modifyTxtBtnWow() {
-	var tempTxt ;
-	if (wowOpen) {
-		tempTxt = "Ouvrir Intro" ;
-		wowOpen = false ;
-	}
-	else {
-		tempTxt = "Fermer Intro" ;
-		wowOpen = true ;
-	}
-	$("#btnWow").empty() ;
-	$("#btnWow").append(tempTxt) ;
 }
 
 function initializeFooter() {
@@ -62,6 +37,6 @@ function initializeFooter() {
 			console.log(isContactPage) ;
 			if (!isHomePage && !isContactPage) {tempLink = notHomeLink+tempLink ;}
 			creaddAElement(null, "footerLink internal white", tempLink, " Contact", "divPlanContact") ;
-		creaddBasicElement("div", null, "row allCenterFlex", "©2021 ÔCampus Tous Droits réservés.", null, "divFooter") ;
+		creaddBasicElement("div", null, "row allCenterFlex", "©2022 ÔCampus Tous Droits réservés.", null, "divFooter") ;
 		creaddBasicElement("div", null, "row allCenterFlex", "Informations légales | Mentions légales | Cookies", null, "divFooter") ;
 }
